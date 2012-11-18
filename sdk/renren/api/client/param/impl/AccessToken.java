c4f2bd87b17f6c74085e610bfe3aaf2930a9fd12
zmy90320@gmail.com <zmy90320@gmail.com@704de19e-2990-137f-9a3d-4fd72104a81e>
2012-11-18 09:22:24 +0000

    Add Renren SDK code

---
+++ sdk/renren/api/client/param/impl/AccessToken.java
@@ -0,0 +1,21 @@
+package sdk.renren.api.client.param.impl;
+
+import sdk.renren.api.client.param.Auth;
+
+public class AccessToken implements Auth {
+    private String key="access_token";
+    private String value;
+    public AccessToken(String value){
+        this.value=value;
+    }
+    public String getKey() {
+        // TODO Auto-generated method stub
+        return key;
+    }
+
+    public String getValue() {
+        // TODO Auto-generated method stub
+        return value;
+    }
+
+}
---
+++ sdk/renren/api/client/utils/Parameters.java
@@ -0,0 +1,8 @@
+package sdk.renren.api.client.utils;
+
+public class Parameters {
+	public static final String USER_AGENT = "Renren Java SDK v2.0";
+	public static final String MD5_USER_AGENT = "49e70de73da2f12a8edf5427201089f8";//Renren Java SDK v2.0��md5ֵ
+
+}
+
